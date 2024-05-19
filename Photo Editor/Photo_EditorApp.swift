d9093cdd2f5c6ad7ec34bf439ec288fe602a3a76
Md Abir Hossain <hossain.mdabir@gmail.com>
2024-05-19 15:59:58 +0600

    Initial Commit

---
+++ Photo Editor/Photo_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_EditorApp.swift
+//  Photo Editor
+//
+//  Created by Md Abir Hossain on 19/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Photo_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
